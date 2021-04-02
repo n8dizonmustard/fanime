db280f52443d9cd0bc674a7ef7bec033add4edd1
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-04-02 13:14:56 -0700

    added login and signup templates. added more for auth in all files.

--- fanime/fanime_app/models.py
+++ fanime/fanime_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,10 +7,11 @@ class Profile(models.Model):
     # avatar = models.
     # fav_list = models.
     # want_list = models.
-    currently_watching_list = models.CharField()
+    currently_watching_list = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Anime(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     rating = models.IntegerField()
     # image = models.
     description = models.TextField()
