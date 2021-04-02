1e03a0a3e55713ee9f309658e3fb441a69304c5d
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-04-02 11:36:03 -0700

    add profile and anime models

--- fanime/fanime_app/models.py
+++ fanime/fanime_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    # avatar = models.
+    # fav_list = models.
+    # want_list = models.
+    currently_watching_list = models.CharField()
+
+class Anime(models.Model):
+    name = models.CharField()
+    rating = models.IntegerField()
+    # image = models.
+    description = models.TextField()
