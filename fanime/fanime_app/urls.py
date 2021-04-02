afd0f1331ef0f7878707de913a18439ba9dc4bd1
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-04-02 14:28:24 -0700

    added signup

--- fanime/fanime_app/urls.py
+++ fanime/fanime_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
+
+    path('accounts/signup/', views.signup, name='signup'),
 ]
