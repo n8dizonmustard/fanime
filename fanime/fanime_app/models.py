2efaa95b6d61579855b137708f985c4a017c5794
Andrew Dimes <andrewdimes@gmail.com>
2021-04-02 21:07:03 -0400

    add button css and add profile/comments to admin

--- fanime/fanime_app/models.py
+++ fanime/fanime_app/models.py
@@ -4,17 +4,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class Anime(models.Model):
-    api_id = models.CharField(max_length=100)
-
-
 class Profile(models.Model):
-    avatar = models.URLField()
-    anime = models.ManyToManyField(Anime)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Comment(models.Model):
     comment = models.CharField(max_length=300)
-    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
