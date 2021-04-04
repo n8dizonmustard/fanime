6830cb466cd655e31916291a69b52e19fc9b3e7f
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-04-04 10:53:11 -0700

    spaced out a function in views

--- fanime/fanime_app/admin.py
+++ fanime/fanime_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Anime, Profile, Comment
 
 # Register your models here.
+admin.site.register(Anime)
+admin.site.register(Profile)
+admin.site.register(Comment)
